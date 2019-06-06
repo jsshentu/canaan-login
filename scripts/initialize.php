206006c326a3d2b9e395cdfb698a45bd2529687e
jsshentu <jshentu@uci.edu>
2019-06-05 23:45:29 -0700

    connect to db

--- scripts/initialize.php
+++ scripts/initialize.php
@@ -1,3 +1,11 @@
 <?php
-  require_once();
+  require_once("connection.php");
+  //require_once("database.php");
+  $db_conn = get_connection();
+
+/*
+  if($db_conn) {
+    echo "connected" . "\n";
+  }
+*/
 ?>
