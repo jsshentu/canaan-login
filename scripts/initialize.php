6ec127622b8109f88d6eccd5314f3087970c419a
jsshentu <jshentu@uci.edu>
2019-06-07 20:50:17 -0700

    renamed the check_password to verify_password

--- scripts/initialize.php
+++ scripts/initialize.php
@@ -1,8 +1,10 @@
 <?php
   require_once("connection.php");
-  //require_once("database.php");
+  require_once("validation.php");
+
   $db_conn = get_connection();
 
+  
 /*
   if($db_conn) {
     echo "connected" . "\n";
