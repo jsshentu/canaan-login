9993647cdd216b04017a10069840fb9a00209d91
jsshentu <jshentu@uci.edu>
2019-06-07 22:48:04 -0700

    updated insert_into_sign_up function

--- scripts/connection.php
+++ scripts/connection.php
@@ -8,16 +8,18 @@
   }
 
   //insert into sign_up table
-  function insert_into_sign_up($email, $password) {
+  function insert_into_sign_up($name, $birthday, $email, $password) {
     global $db_conn;
     $sql = <<<EOD
-    INSERT INTO sign_up (email, password, v_password)
-    VALUES (:email, :password, :v_password)
+    INSERT INTO sign_up (name, birthday, email, password)
+    VALUES (:name, STR_TO_DATE(:$birthday, '%d/%m/%Y'), :email, :password)
 EOD;
 
     $insert = $db_conn->prepare($sql);
+    $insert->bindParam(':name', $name, PDO::PARAM_STR);
+    $insert->bindParam(':birthday', $birthday, PDO::PARAM_STR);
     $insert->bindParam(':email', $email, PDO::PARAM_STR);
     $insert->bindParam(':password', $password);
-    $insert->execute();
+    return $insert->execute() ? true : false;
   }
 ?>
