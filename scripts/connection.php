271c65fa1122636fe84235ba9158e414e0f37628
jsshentu <jshentu@uci.edu>
2019-06-26 23:06:45 -0700

    insert into register function done

--- scripts/connection.php
+++ scripts/connection.php
@@ -12,14 +12,30 @@
     global $db_conn;
     $sql = <<<EOD
     INSERT INTO sign_up (name, birthday, email, password)
-    VALUES (:name, STR_TO_DATE(:$birthday, '%d/%m/%Y'), :email, :password)
+    VALUES (:name, STR_TO_DATE(:birthday, '%Y-%m-%d'), :email, :password)
 EOD;
 
     $insert = $db_conn->prepare($sql);
     $insert->bindParam(':name', $name, PDO::PARAM_STR);
     $insert->bindParam(':birthday', $birthday, PDO::PARAM_STR);
     $insert->bindParam(':email', $email, PDO::PARAM_STR);
-    $insert->bindParam(':password', $password);
+    $insert->bindParam(':password', $password, PDO::PARAM_STR);
+    return $insert->execute() ? true : false;
+  }
+  
+  function insert_into_register($name, $birthday, $email, $cellphone, $topic){
+    global $db_conn;
+    $sql = <<<EOD
+    INSERT INTO register (name, birthday, email, phone, topic)
+    VALUES (:name, STR_TO_DATE(:birthday, '%Y-%m-%d'), :email, :phone, :topic)
+EOD;
+
+    $insert = $db_conn->prepare($sql);
+    $insert->bindParam(':name', $name, PDO::PARAM_STR);
+    $insert->bindParam(':birthday', $birthday, PDO::PARAM_STR);
+    $insert->bindParam(':email', $email, PDO::PARAM_STR);
+    $insert->bindParam(':phone', $cellphone, PDO::PARAM_STR);
+    $insert->bindParam(':topic', $topic, PDO::PARAM_STR);
     return $insert->execute() ? true : false;
   }
 ?>
