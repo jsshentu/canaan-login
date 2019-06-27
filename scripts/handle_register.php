3b44d0083b892f24a5d00a17a8e790dbb4055eba
jsshentu <jshentu@uci.edu>
2019-06-26 23:18:24 -0700

    check unique email updated

--- scripts/handle_register.php
+++ scripts/handle_register.php
@@ -12,22 +12,17 @@
     if(!check_email_format($email)){
       array_push($errors, "Email的格式不正确");
     }
-    /*
+    
     if(!check_unique_email("register", $email)){
       array_push($errors, "该邮箱已经注册，请使用其他邮箱注册");
     }
-    */
-
-    $output = check_unique_email("register", $email);
-    echo $output;
+    
     if(empty($errors)) {
       $result = insert_into_register($name, $birthday, $email, $cellphone, $topic);
       if($result){
-        //echo "<h1>" . $output . "</h1>";
         header('Location: successful.html');
         exit;
       } else {
-        //echo "<h1>" . $output . "</h1>";
         header('Location: failed.html');
         exit;
       }
